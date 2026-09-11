1c2559e34cb3ae058a7188aa0929c56c2f017605
Matheus Castello <matheus@castello.eng.br>
2026-09-10 23:10:06 -0300

    scripts: Ensure virtual environment is used for pip
    These will never work on modern Debian based systems without using a
    virtual environment for pip.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- Silicon/NVIDIA/scripts/prepare_stuart.sh
+++ Silicon/NVIDIA/scripts/prepare_stuart.sh
@@ -26,12 +26,16 @@ if [[ -z "${UEFI_SKIP_VENV}" ]]; then
   find_venv_activate
   if [ ! -e ${VENV_ACTIVATE} ]; then
     _msg "Creating Python virtual environment in `pwd`/venv..."
-    virtualenv -p python3 venv
+    # Use python3 -m venv instead of virtualenv for better compatibility
+    python3 -m venv venv
     find_venv_activate
     . ${VENV_ACTIVATE}
     _msg "Installing required Python packages..."
-    pip install ${UEFI_PIP_INSTALL_ARGS} --upgrade -r edk2/pip-requirements.txt
-    pip install ${UEFI_PIP_INSTALL_ARGS} --upgrade kconfiglib
+    # Upgrade pip first to ensure we have latest version
+    pip install --upgrade pip
+    # Force installation in the virtual environment to avoid system conflicts
+    pip install --force-reinstall ${UEFI_PIP_INSTALL_ARGS} --upgrade -r edk2/pip-requirements.txt
+    pip install --force-reinstall ${UEFI_PIP_INSTALL_ARGS} --upgrade kconfiglib
   else
     _msg "Activating Python virtual environment."
     find_venv_activate
